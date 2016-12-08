870f92901872ecd739df9f35c97e728f2214a222
Itega <Itega@users.noreply.github.com>
2016-12-08 14:16:30 +0100

    Avions

--- C/avions.c
+++ C/avions.c
@@ -15,20 +15,60 @@ typedef struct avion avion;
     	int sens;
 	};
 
-void changeAvion(int tableau[][], int sens, avion avion)
+void changeAvion(int **tableau, int sens, avion avion)
 {
+	FILE *fp;
 	int i, j;
+	char line[1024];
+	char *p = NULL;
+	char chemin[128];
 	//ignorer 2 premiere lignes + #
-	if(sens == 0)//Vers le haut
+	switch(sens)
 	{
-		for(i=0; i<6; i++)
+		case 0:
+			strcpy(chemin, "avion_bas.pbm");
+			break;
+		case 1:
+			strcpy(chemin, "avion_gauche.pbm");
+			break;
+		case 2:
+			strcpy(chemin, "avion_haut.pbm");
+			break;
+		case 3:
+			strcpy(chemin, "avion_droite.pbm");
+			break;
+	}	
+	fp = fopen(chemin, "r");
+	i = 0;
+	while(i<2)
+	{
+		if(fgets(line,sizeof(line),fp)!= NULL) //Si la ligne n'est pas vide
 		{
-			for(j=0; j<6; j++)
+			if(strncmp(line, "#", 1) != 0) //On ignore les commentaires
 			{
-				tableau[avion.x+i][avion.y+j];
+				i++;
 			}
 		}
 	}
+	i = 0;
+	j = 0;
+	while(fgets(line,sizeof(line),fp)!= NULL)
+	{
+		p = strtok(line, " ");
+		while(p != NULL)
+		{
+			if(strncmp(p, "1", 1) == 0)
+			{
+				tableau[avion.x+j][avion.y+i] = 1;
+			}
+			p = strtok(NULL, " ");
+			i++;
+		}
+		j++;
+		i = 0;
+	}
+	fclose(fp);
+	
 }
 
 int main(int argc, char *argv[])
@@ -36,6 +76,9 @@ int main(int argc, char *argv[])
 	struct winsize w;
 
 	avion avion;
+
+	avion.x = 10;
+	avion.y = 10;
 
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
     int col = w.ws_col;
@@ -55,12 +98,12 @@ int main(int argc, char *argv[])
 	 		tableau[i][j] = 0;
 	 	}
 	}
-	changeAvion(tableau, 0, avion);
+	changeAvion(tableau, 2, avion);
 	for(i=0; i<row; i++)
 	{
 		for(j=0; j<col; j++)
 		{
-	 		printf("%s", tableau[i][j]);
+	 		printf("%d", tableau[i][j]);
 	 	}
 	}
 
