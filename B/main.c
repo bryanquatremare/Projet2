a2468951a4b0a8299478b72b99a3582e35133048
Bryan Quatremare <tiplouf76320@live.fr>
2016-12-08 14:22:06 +0100

    Ajout du main et des bibliothèques du deuxième screensaver

---
+++ B/main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h> // routines standards
+#include <stdio.h> // traitement entrées/sortie
+#include <string.h> // tratement de chaine de caractères
+#include <sys/ioctl.h> // control devices (like terminals)
+#include <unistd.h> // type et constante symboles standard
+#include <math.h> // traitement mathématique
+#include <errno.h> // traitement des erreurs
+#include <sys/wait.h> // définitions d'attentes
+#include <sys/types.h> // définitions de temps
+
+int main(int argc, int *argv[])
+{
+	
+}
