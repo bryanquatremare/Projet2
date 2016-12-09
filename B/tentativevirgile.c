ad43260baa010d49e5a2fce8e65fa2d8d45991c5
Morrosis <marandlycee@gmail.com>
2016-12-09 16:26:08 +0100

    Correction bugs
    Allocation mémoire ajoutée

--- B/tentativevirgile.c
+++ B/tentativevirgile.c
@@ -3,19 +3,24 @@
 #include <string.h>
 #include <time.h> //ajout librairie pour utiliser les fontions manipulatrices de dates locales et systèmes
 
-void quelheure()
+void heurelocale(int *heure, int *minute, int *seconde)
 {
+	char h[2];
+	char m[2];
+	char s[2];
+	int i, j, k;
 	time_t timer; // création de timer avec un paramètre spécial time_t afin qu'il puisse stocker toute la date
 	struct tm * t; // création de t qui pourra contenir la date sous la forme d'une structure
 
 	time(&timer); //réception de la date dans timer
 
-
 	t = localtime(&timer); // réception de la date contenue dans timer dans la structure t
-	printf("%02uh %02um %02us\n", t->tm_hour, t->tm_min, t->tm_sec); // affichage de l'heure les minutes et les secondes avec l'appel %02u
-	char *heure = t->tm_hour;
-	char *minute = t->tm_min;
-	char *seconde = t->tm_sec;
+	sprintf(h,"%02u",t->tm_hour);
+	sprintf(m,"%02u",t->tm_min);
+	sprintf(s,"%02u",t->tm_sec);
+	*heure = atoi(h);
+	*minute = atoi(m);
+	*seconde = atoi(s);	
 }
 
 void lectureligne(char ligne[70])
@@ -36,14 +41,18 @@ void lectureligne(char ligne[70])
 int main()
 {
 	FILE *f, *g, *h, *k, *l, *m;
-	int *heure = NULL, *minute = NULL, *seconde = NULL;
+	int *heure = malloc(2 * sizeof(int *));
+	int *minute = malloc(2 * sizeof(int *));
+	int *seconde = malloc(2 * sizeof(int *));
+	
 	char type[10];
 	char *tok = NULL;
 	int tab[2];
 	char ligne[70];
 	
-	quelheure;
-	printf("%s %s %s", *heure, *minute, *seconde);
+	heurelocale(heure, minute, seconde);
+	printf("%02u %02u %02u", *heure, *minute, *seconde);
+	printf("\n");
 
 	f = fopen("test_coeur.pbm", "r");
 	g = fopen("test_coeur.pbm", "r");
@@ -97,6 +106,9 @@ int main()
 	fclose(k);
 	fclose(l);
 	fclose(m);
-
+	
+	free(heure);
+	free(minute);
+	free(seconde);
 	return 0;
 }
--- B/veille2.c
+++ B/veille2.c
@@ -1,15 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h> //ajout librairie pour utiliser les fontions manipulatrices de dates locales et systèmes
+#include <string.h>
 
-void main()
+void heurelocale(int *heure, int *minute, int *seconde)
 {
+	char h[2];
+	char m[2];
+	char s[2];
 	time_t timer; // création de timer avec un paramètre spécial time_t afin qu'il puisse stocker toute la date
 	struct tm * t; // création de t qui pourra contenir la date sous la forme d'une structure
 
 	time(&timer); //récéption de la date dans timer
 
-
 	t = localtime(&timer); // réception de la date contenue dans timer dans la structure t
 	printf("%02uh %02um %02us\n", t->tm_hour, t->tm_min, t->tm_sec); // affichage de l'heure les minutes et les secondes avec l'appel %02u
+	sprintf(h,"%02u",t->tm_hour);//récupération de l'heure dans le char "heure"
+	sprintf(m,"%02u",t->tm_min);//récupération des minutes dans le char "minute"
+	sprintf(s,"%02u",t->tm_sec);//récupération des secondes dans le char "seconde"
+	printf("%s %s %s\n",h,m,s);
+	*heure = h;
+	*minute = m;
+	*seconde = s;
 }
