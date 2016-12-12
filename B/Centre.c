c304ed3cb763095731c206e0afa083d6244a5f3d
Bryan Quatremare <tiplouf76320@live.fr>
2016-12-12 10:33:24 +0100

    Création d'une fonction pour récupérer la taille du terminal

---
+++ B/Centre.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // traitement entrées/sortie
+#include <string.h> // traitement des chaines de caractère
+#include <sys/ioctl.h> // contrôle des périphériques
+
+void winsize () // fonction qui récupère les dimmensions de la fenêtre
+{
+	int longueur;
+	int largeur;
+
+
+}
