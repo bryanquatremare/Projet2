8b07b1260212716b613c4b42aff21e5b7962d572
Morrosis <marandlycee@gmail.com>
2016-12-12 16:46:33 +0100

    Test Agrandissement 0.1

--- B/main.c
+++ B/main.c
@@ -28,13 +28,14 @@ int main()
 	int *lignes = malloc(sizeof(int *)); // création du pointeur qui contiendra le nombre de lignes
 	int *colonnes = malloc(sizeof(int *));	// création du pointeur qui contiendra le nombre de colonnes
 	int n; // variable compteur (pour parcourir toute la ligne)
-	int nc;
-	int quelprojet;
-	int nbradd = 1;
-
+	int nc = 0;
+	char derniereligne[500];
+	int nbradd = 3;
+	int i = 0;
+	int w = 0;
 	taillefen(colonnes, lignes); // on appelle la fonction pour connaître la taille de la fenêtre
 
-	f = fopen("test_coeur.pbm", "r");	// on met l'adresse du fichier dans le pointeur
+	f = fopen("8.pbm", "r");	// on met l'adresse du fichier dans le pointeur
 
 	for(n=0; n<2;)	//tant que n est inferieur a 2 (qu'on a pas trouvé 2 lignes sans commentaires)
 	{
@@ -62,25 +63,77 @@ int main()
 
 		while(fgets(ligne, 100,f)!= NULL) // tant que la ligne existe
 		{
-			n++;
+			
+			nc = 0;
 			if(strncmp(ligne, "#", 1)) // on vérifie que le premier caractère n'est pas "#" (un commentaire), sinon on change de ligne
 			{
-				tok = strtok(ligne, " "); // découpage du premier morceau jusqu'au séparateur (" ") (permet la suppression des espace)
-				while(tok != NULL)	// tant que le token n'est pas vide (tant qu'on a pas atteint la fin de la ligne)
-	    		{
-					if(!(strncmp(tok, "1", 1))) // si on lit 1 dans le .pbm
+				/**/if(n == 3 | n == nbradd+4) //si nous en sommes à la 3 ème ou 4+nbradd ème ligne du chiffre
+				{
+					if (nbradd > 1)  // si nous sommes à un format supérieur à 5 par 3
 					{
-						printf("█");	// on affiche le caractère █
+						i = 0;
+						for(i = 0; i < nbradd;) //tant que nous n'avon pas ajouté suffisemment de lignes 
+						{
+							printf("%s\n",derniereligne); //print la dernière ligne entrée
+							i++;
+							n++;
+						}
+						w = 1;
 					}
-					else	// si on lit un 0 dans le .pbm
-					{
-						printf(" ");	// sinon afficher un espace
-					}
-					tok = strtok(NULL, " "); // on passe au token suivant
 				}
-				printf("\n");	// retour à la ligne à la fin de chaque lignes
+				if(w == 0)
+				{/**/
+					strcpy(derniereligne,""); //réinitialisation de derniereligne
+					tok = strtok(ligne, " "); // découpage du premier morceau jusqu'au séparateur (" ") (permet la suppression des espace)
+					while(tok != NULL)	// tant que le token n'est pas vide (tant qu'on a pas atteint la fin de la ligne)
+					{
+						if(!(strncmp(tok, "1", 1))) // si on lit 1 dans le .pbm
+						{
+							/**/if(nc == 2) //si on est au deuxieme caractere de la ligne
+							{
+								i = 0;
+								for(i = 0; i < nbradd;) //tant que nous n'avons pas ajouté tout les █ nécessaires
+								{
+									printf("█");
+									i++;
+									strcat(derniereligne,"█"); //entrer █ dans la chaine derniereligne
+									nc++; // on passe au caractère suivant
+								}
+							}/**/
+							else
+							{
+								printf("█");	// on affiche le caractère █
+								/**/strcat(derniereligne,"█"); //entrer █ dans la chaine derniereligne
+								nc++;/**/  // on passe au caractère suivant
+							}
+						}
+						else	// si on lit un 0 dans le .pbm
+						{
+							/**/if(nc == 2) //si on est au deuxieme caractere de la ligne
+							{
+								i = 0;
+								for(i = 0; i < nbradd;) //tant que nous n'avons pas ajouté tout les   nécessaires
+								{
+									printf(" ");
+									i++;
+									strcat(derniereligne," "); //entrer   dans la chaine derniereligne
+									nc++; // on passe au caractère suivant
+								}
+							}/**/
+							else
+							{
+								printf(" ");	// sinon afficher un espace
+								/**/strcat(derniereligne," "); //entrer   dans la chaine derniereligne
+								nc++;/**/  // on passe au caractère suivant
+							}
+						}
+						tok = strtok(NULL, " "); // on passe au token suivant
+					}
+					printf("\n");	// retour à la ligne à la fin de chaque lignes
+				/**/}/**/
 			}
-		}
+			n++;
+		/**/}/**/
 	printf("Le fichier PBM est de type %s\n", type);
 	printf("Largeur = %d Longueur = %d.\n", dim[0], dim[1]);
 	printf("Ce terminal possède %d colonnes et %d lignes.\n", *colonnes, *lignes);
