7419509b94694cc7e6240654ef5fa09e83ad54d1
Morrosis <marandlycee@gmail.com>
2016-12-13 10:21:02 +0100

    Ajout d'adaptation de l'image selon la fenêtre

--- B/main.c
+++ B/main.c
@@ -10,13 +10,11 @@ int main()
 	FILE *f;		//création du pointeur vers le fichier
 	char *ligne = malloc(sizeof(char) * 1024);	// création de la chaine de caractère qui stockera la ligne
 	char type[10];	// création de la variable contenant le type de PBM
+	char tampon[100];
 	int dim[2];	//création d'un tableau
 	int *lignes = malloc(sizeof(int *)); // création du pointeur qui contiendra le nombre de lignes
 	int *colonnes = malloc(sizeof(int *));	// création du pointeur qui contiendra le nombre de colonnes
 	int n; // variable compteur (pour parcourir toute la ligne)
-	int nc;
-	int quelprojet;
-	int nbradd = 1;
 
 	taillefen(colonnes, lignes); // on appelle la fonction pour connaître la taille de la fenêtre
 
@@ -42,14 +40,29 @@ int main()
 		if(ligne[n] == '1')	// si on lit 1 dans le .pbm
 		{
 			printf("█");	// on affiche le caractère █
+			if(*lignes >= 35 & *colonnes >=100)
+			{
+				printf("█");
+				strcat(tampon,"██");
+			}
 		}
 		else if(ligne[n] == '2') //sinon si on lit 2 dans le pbm
 		{
 			printf("\n"); // faire un retour à la ligne
+			if(*lignes >= 35 & *colonnes >=100)
+			{
+				printf("%s\n",tampon);
+				strcpy(tampon,"");
+			}
 		}
 		else	// sinon si on lit un 0 dans le .pbm
 		{
 			printf(" ");	// afficher un espace
+			if(*lignes >= 35 & *colonnes >=100)
+			{
+				printf(" ");
+				strcat(tampon,"  ");
+			}
 		}
 	}
 
