3aa0c2f5c9f101e34eed3c172358920cc33a3105
Bryan Quatremare <tiplouf76320@live.fr>
2016-12-13 11:02:36 +0100

    Ajout de commentaires au fichier centre.c

--- B/centre.c
+++ B/centre.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-void centre(int *axeconsole, int axepbm)
+void centre(int *axeconsole, int axepbm) // fonction pour centrer l'image
 {
-int n = 1;
-while(n != (*axeconsole/2)-(axepbm/2))
+int n = 1; // compteur de saut de ligne
+while(n != (*axeconsole/2)-(axepbm/2)) // tant que la ligne actuelle (n) n'est pas égale au centre
 	{	
-		printf("\n");
-		n++;
+		printf("\n"); // sauter une ligne
+		n++; // ajouter 1 à la ligne actuelle
 	}
 
-	if((*axeconsole/2)-(axepbm/2) % 2 != 0)
+	if((*axeconsole/2)-(axepbm/2) % 2 != 0) // si le nombre total de sauts de lignes est impair
 	{
-		printf("\n");
+		printf("\n"); // rajouter un saut de ligne
 	}
 }
