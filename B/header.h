444ae20850267831232eff27f39f5e3360c24271
Bryan Quatremare <tiplouf76320@live.fr>
2016-12-13 11:49:42 +0100

    Revert "Renommm"
    This reverts commit b19724519de2d0e47821095cc2e7a6b5e6570d9c.

--- B/header.h
+++ B/header.h
@@ -1,4 +1,4 @@
 void readPBM(char *path, int *size, char *type, char *output);
 void taillefen(int *colonnes, int *lignes);
-void centreVert(int *axelig, int *axecol, int axepbm);
+void centre(int *axelig, int *axecol, int axepbm);
 void heurelocale(int *heuredizaine, int *heureunite, int *minutedizaine, int *minuteunite, int *secondedizaine, int *secondeunite);
