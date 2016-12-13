0e1f8e34eef6644182b623c6021340387508eeeb
Morrosis <marandlycee@gmail.com>
2016-12-13 11:35:29 +0100

    Debug centrage vis-a-vis de la taille de la fenetre

--- B/centre.c
+++ B/centre.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 
-void centre(int *axeconsole, int axepbm) // fonction pour centrer l'image
+void centre(int *axelig, int *axecol, int axepbm) // fonction pour centrer l'image
 {
 int n = 1; // compteur de saut de ligne
-while(n != (*axeconsole/2)-(axepbm/2)) // tant que la ligne actuelle (n) n'est pas égale au centre
-	{	
-		printf("\n"); // sauter une ligne
-		n++; // ajouter 1 à la ligne actuelle
-	}
-
-	if((*axeconsole/2)-(axepbm/2) % 2 != 0) // si le nombre total de sauts de lignes est impair
+	if(*axecol >= 100 & *axelig >=35)
 	{
-		printf("\n"); // rajouter un saut de ligne
+		while(n != (*axelig/2)-(axepbm)) // tant que la ligne actuelle (n) n'est pas égale au centre
+		{	
+			printf("\n"); // sauter une ligne
+			n++; // ajouter 1 à la ligne actuelle
+		}
+
+		if((*axelig/2)-(axepbm) % 2 != 0) // si le nombre total de sauts de lignes est impair
+		{
+			printf("\n"); // rajouter un saut de ligne
+		}
+	}
+	else
+	{
+		while(n != (*axelig/2)-(axepbm/2)) // tant que la ligne actuelle (n) n'est pas égale au centre
+		{	
+			printf("\n"); // sauter une ligne
+			n++; // ajouter 1 à la ligne actuelle
+		}
+
+		if((*axelig/2)-(axepbm/2) % 2 != 0) // si le nombre total de sauts de lignes est impair
+		{
+			printf("\n"); // rajouter un saut de ligne
+		}
 	}
 }
