eb6fe62fff4a93c7d5acb93e9bf42acdef9295a7
Itega <Itega@users.noreply.github.com>
2016-12-15 09:35:25 +0100

    Screen Saver 1

---
+++ static.h
@@ -0,0 +1,6 @@
+void strToTable(char **table, char *str, int col, int row, int *size);//Input : table[][], str, size[2]
+void printTable(char **table, int col, int row);//Input : table[][], size[2]
+
+int **initTable(char **table, int col, int row);//Input : table[][], size[2]
+
+char getch();
