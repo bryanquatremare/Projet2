b4d8d1664da94fdfc1584e2e8e621262b5c3aeb5
Itega <Itega@users.noreply.github.com>
2016-12-15 09:27:21 +0100

    Delete lecture.h

--- C/lecture.h
+++
@@ -1,5 +0,0 @@
-void readPBM(char *path, int size[], char *type, char *output);//Input path, size[2], type, output
-void strToTable(int **table, char *str, int size[]);//Input : table[][], str, size[2]
-void printTable(int **table, int size[]);//Input : table[][], size[2]
-
-int **initTable(int **table, int size[]);//Input : table[][], size[2]
